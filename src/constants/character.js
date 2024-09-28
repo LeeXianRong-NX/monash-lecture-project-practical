01208f45f1f5e2d89f5ad277b93506759d52aa89
Xian Rong Lee <xianrong@netizenexperience.com>
2024-09-28 22:28:34 +0800

    chore(constants): add constant values for character self introduction message and superpower value

---
+++ src/constants/character.js
@@ -0,0 +1,4 @@
+// Character self introduction message templates
+exports.PERSON_SELF_INTRO_MSG = "Hello! My name is {{name}}.";
+exports.SUPERHERO_SELF_INTRO_MSG = "Do not fear! The hero {{name}} is here!";
+exports.VILLAIN_SELF_INTRO_MSG = "Ah ha ha ha! It is I - the great {{name}}!";
---
+++ src/constants/superpower.js
@@ -0,0 +1,4 @@
+// Superpower power values
+exports.SUPERPOWER_ROCK = "✊";
+exports.SUPERPOWER_SCISSORS = "✌️";
+exports.SUPERPOWER_PAPER = "✋";
