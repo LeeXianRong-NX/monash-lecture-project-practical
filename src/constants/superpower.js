d31e929295b41491927be0a4b88d40ab98651dd0
Xian Rong Lee <xianrong@netizenexperience.com>
2024-10-01 18:01:37 +0800

    chore(constants): add superpower info constant values

--- src/constants/superpower.js
+++ src/constants/superpower.js
@@ -2,3 +2,18 @@
 exports.SUPERPOWER_ROCK = "✊";
 exports.SUPERPOWER_SCISSORS = "✌️";
 exports.SUPERPOWER_PAPER = "✋";
+
+// Super strength superpower
+exports.SUPER_STRENGTH_NAME = "Super Strength";
+exports.SUPER_STRENGTH_DESCRIPTION = "Wield extraordinary power";
+exports.SUPER_STRENGTH_POWER = this.SUPERPOWER_ROCK;
+
+// Flight superpower
+exports.FLIGHT_NAME = "Flight";
+exports.FLIGHT_DESCRIPTION = "Soar freely in the skies";
+exports.FLIGHT_POWER = this.SUPERPOWER_SCISSORS;
+
+// Laser vision superpower
+exports.LASER_VISIONS_NAME = "Laser Vision";
+exports.LASER_VISIONS_DESCRIPTION = "Incinerate all that you can see";
+exports.LASER_VISIONS_POWER = this.SUPERPOWER_PAPER;
