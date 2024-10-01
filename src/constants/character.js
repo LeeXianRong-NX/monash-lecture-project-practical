be2dc21c6d33b3225f14f322ef3b3bf8206151d4
Xian Rong Lee <xianrong@netizenexperience.com>
2024-10-01 18:03:50 +0800

    chore(constants): add default villain info constants

--- src/constants/character.js
+++ src/constants/character.js
@@ -2,3 +2,7 @@
 exports.PERSON_SELF_INTRO_MSG = "Hello! My name is {{name}}.";
 exports.SUPERHERO_SELF_INTRO_MSG = "Do not fear! The hero {{name}} is here!";
 exports.VILLAIN_SELF_INTRO_MSG = "Ah ha ha ha! It is I - the great {{name}}!";
+
+// Default Villain
+exports.DEFAULT_VILLAIN_PERSON_NAME = "Tim Tom";
+exports.DEFAULT_VILLAIN__NAME = "Super Evil Tim";
