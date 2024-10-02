fee6c4e3999e244e8ffb40c2e02264d800f23cc7
Xian Rong Lee <xianrong@netizenexperience.com>
2024-10-02 14:07:59 +0800

    feat(helper-game): add helper methods related to game logic

---
+++ src/helper/game/index.js
@@ -0,0 +1,89 @@
+const {
+  GAME_TURN_DRAW_RESULT,
+  GAME_TURN_LOSE_RESULT,
+  GAME_TURN_WIN_RESULT,
+} = require("../../constants/game");
+const {
+  SUPERPOWER_ROCK,
+  SUPERPOWER_SCISSORS,
+  SUPERPOWER_PAPER,
+} = require("../../constants/superpower");
+
+/**
+ * Check if character has landed critical hit by comparing selected move and own superpower value
+ * @param {import("../../classes/superpower/types").PowerValue} move - Power value of character's selected move
+ * @param {import("../../classes/superpower/types").PowerValue} power - Power value of character's superpower
+ * @returns {boolean}
+ */
+function isCriticalHit(move, power) {
+  return move === power;
+}
+
+/**
+ * Check if turn has any critical hits from either superhero or villain
+ * @param {boolean} isHeroCrit - Boolean value if superhero has landed critical hit
+ * @param {boolean} isVillainCrit - Boolean value if villain has landed critical hit
+ * @returns {boolean}
+ */
+function turnHasCriticalHit(isHeroCrit, isVillainCrit) {
+  return isHeroCrit || isVillainCrit;
+}
+
+/**
+ * Check if player has won the game by comparing superhero and villain HP
+ * @param {number} heroHp - Superhero HP number value
+ * @param {number} villainHp - Villain HP number value
+ * @returns {boolean | null}
+ */
+function isPlayerWinner(heroHp, villainHp) {
+  if (heroHp > 0 && villainHp <= 0) return true;
+  if (heroHp <= 0 && villainHp > 0) return false;
+  return null;
+}
+
+/**
+ * Calculate result for game turn by comparing moves by superhero and villain
+ * @param {import("../../classes/superpower/types").PowerValue} heroMove - Superhero move power value
+ * @param {import("../../classes/superpower/types").PowerValue} villainMove - Villain move power value
+ * @returns {import("./types").gameTurnResult}
+ */
+function getTurnResult(heroMove, villainMove) {
+  if (heroMove === villainMove) return GAME_TURN_DRAW_RESULT;
+
+  if (
+    (heroMove === SUPERPOWER_ROCK && villainMove === SUPERPOWER_SCISSORS) ||
+    (heroMove === SUPERPOWER_SCISSORS && villainMove === SUPERPOWER_PAPER) ||
+    (heroMove === SUPERPOWER_PAPER && villainMove === SUPERPOWER_ROCK)
+  )
+    return GAME_TURN_WIN_RESULT;
+
+  return GAME_TURN_LOSE_RESULT;
+}
+
+/**
+ * Get string params to interpolate into turn end UI string
+ * @param {import("./types").turnEndUIInterpolatePayload} payload - Payload to build string params
+ * @returns {import("./types").turnEndUIStringParams}
+ */
+function getTurnEndUIStringParams(payload) {
+  return {
+    turnNum: payload.turnNum,
+    heroMove: payload.heroMove,
+    villainMove: payload.villainMove,
+    turnResult: payload.turnResult,
+    critMsg: payload.hasCriticalHit ? "CRITICAL HIT" : null,
+    heroName: payload.superhero.getName(),
+    villainName: payload.villain.getName(),
+    heroHp: payload.superhero.getHp(),
+    villainHp: payload.villain.getHp(),
+    winnerName: payload.winnerName,
+  };
+}
+
+module.exports = {
+  isCriticalHit,
+  turnHasCriticalHit,
+  isPlayerWinner,
+  getTurnResult,
+  getTurnEndUIStringParams,
+};
---
+++ src/helper/game/types.js
@@ -0,0 +1,37 @@
+const Superhero = require("../../classes/superhero");
+const Villain = require("../../classes/villain");
+
+/**
+ * String values for game turn result
+ * @typedef {"WIN" | "LOSE" | "DRAW"} gameTurnResult
+ */
+
+/**
+ * Object as function arg to build string interpolation payload for turn end UI string
+ * @typedef {Object} turnEndUIInterpolatePayload
+ * @property {number} turnNum
+ * @property {string} heroMove
+ * @property {string} villainMove
+ * @property {string} turnResult
+ * @property {boolean} hasCriticalHit
+ * @property {Superhero} superhero
+ * @property {Villain} villain
+ * @property {string | null} winnerName
+ */
+
+/**
+ * Parameter object arg for interpolate string function
+ * @typedef {Object} turnEndUIStringParams
+ * @property {number} turnNum
+ * @property {string} heroMove
+ * @property {string} villainMove
+ * @property {string} turnResult
+ * @property {string | null} critMsg
+ * @property {string} heroName
+ * @property {string} villainName
+ * @property {number} heroHp
+ * @property {number} villainHp
+ * @property {string | null} winnerName
+ */
+
+module.exports = {};
