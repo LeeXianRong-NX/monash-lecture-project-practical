912047a07c0135593d01fbab69c6ae56de627524
Xian Rong Lee <xianrong@netizenexperience.com>
2024-10-02 15:42:10 +0800

    fix(helper-superpower): add option to return null if invalid key provided

--- src/helper/superpower/index.js
+++ src/helper/superpower/index.js
@@ -4,6 +4,8 @@ const {
   SUPERPOWER_2_KEY,
   POWER_MOVE_1_KEY,
   POWER_MOVE_2_KEY,
+  SUPERPOWER_3_KEY,
+  POWER_MOVE_3_KEY,
 } = require("../../constants/keys");
 const {
   SUPER_STRENGTH_NAME,
@@ -13,11 +15,11 @@ const {
   FLIGHT_DESCRIPTION,
   FLIGHT_POWER,
   LASER_VISION_NAME,
-  SUPER_STRENGTH_DESCRIPTION,
-  SUPER_STRENGTH_POWER,
   SUPERPOWER_ROCK,
   SUPERPOWER_SCISSORS,
   SUPERPOWER_PAPER,
+  LASER_VISIONS_DESCRIPTION,
+  LASER_VISIONS_POWER,
 } = require("../../constants/superpower");
 
 // Preset Superpower objects
@@ -33,37 +35,54 @@ const FLIGHT_SUPERPOWER = new Superpower(
 );
 const LASER_VISION_SUPERPOWER = new Superpower(
   LASER_VISION_NAME,
-  SUPER_STRENGTH_DESCRIPTION,
-  SUPER_STRENGTH_POWER
+  LASER_VISIONS_DESCRIPTION,
+  LASER_VISIONS_POWER
 );
+
+const PRESET_SUPERPOWERS = {
+  SUPER_STRENGTH: SUPER_STRENGTH_SUPERPOWER,
+  FLIGHT: FLIGHT_SUPERPOWER,
+  LASER_VISION: LASER_VISION_SUPERPOWER,
+};
 
 /**
  * Get corresponding Superpower object from selected key when user chooses superpower
  * @param {import("./types").superpowerKeySelection} key
- * @returns {Superpower}
+ * @returns {Superpower | null}
  */
 function getSuperpowerFromKeySelection(key) {
-  return key === SUPERPOWER_1_KEY
-    ? SUPER_STRENGTH_SUPERPOWER
-    : key === SUPERPOWER_2_KEY
-    ? FLIGHT_SUPERPOWER
-    : LASER_VISION_SUPERPOWER;
+  switch (key) {
+    case SUPERPOWER_1_KEY:
+      return PRESET_SUPERPOWERS.SUPER_STRENGTH;
+    case SUPERPOWER_2_KEY:
+      return PRESET_SUPERPOWERS.FLIGHT;
+    case SUPERPOWER_3_KEY:
+      return PRESET_SUPERPOWERS.LASER_VISION;
+    default:
+      return null;
+  }
 }
 
 /**
  * Get corresponding Superpower power value from selected key when user chooses move
  * @param {import("./types").superpowerMoveKeySelection} key
- * @returns {import("../../classes/superpower/types").PowerValue}
+ * @returns {import("../../classes/superpower/types").PowerValue | null}
  */
 function getPowerFromKeySelection(key) {
-  return key === POWER_MOVE_1_KEY
-    ? SUPERPOWER_ROCK
-    : key === POWER_MOVE_2_KEY
-    ? SUPERPOWER_SCISSORS
-    : SUPERPOWER_PAPER;
+  switch (key) {
+    case POWER_MOVE_1_KEY:
+      return SUPERPOWER_ROCK;
+    case POWER_MOVE_2_KEY:
+      return SUPERPOWER_SCISSORS;
+    case POWER_MOVE_3_KEY:
+      return SUPERPOWER_PAPER;
+    default:
+      return null;
+  }
 }
 
 module.exports = {
+  PRESET_SUPERPOWERS,
   getSuperpowerFromKeySelection,
   getPowerFromKeySelection,
 };
